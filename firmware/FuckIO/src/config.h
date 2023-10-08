6122cb71ab029b4f4e72b7564636a1adde45bda0
elims <46279009+theelims@users.noreply.github.com>
2023-10-08 20:08:47 +0200

    Updates

--- firmware/FuckIO/src/config.h
+++ firmware/FuckIO/src/config.h
@@ -1,21 +1,124 @@
 #pragma once
 
-// Pin Definitions
-#define SERVO_PULSE       4
-#define SERVO_DIR         16
-#define SERVO_ENABLE      17
-#define SERVO_ENDSTOP     25
-#define SERVO_ALARM       26
-#define PWM               21
-#define STATUS_PIN        22        // IoTWebConf WiFi Status Pin
-#define BUTTON_PIN        23        // IoTWebConf Button (for restoring default WiFi AP)
+// General Settings:
+#define HOSTNAME "fuckio"      // Hostname used for DNS
+#define WIFI_AP_NAME "FuckIO"  // Name of the Wifi the AP creates
+#define AP_PASSWORD "12345678" // Default password of the AP
+#define SERIAL_BAUDS 115200    // Default baud rate for serial communication over USB
+#define RSSI_UPDATES 500       // Intervall in ms at which the RSSI value should be send as MQTT message
 
-// Housekeeping Settings:
-#define CONFIG_VERSION      "fio00.01"      // Version of the configuration
-#define THINGNAME           "FuckIO"        // Inital Thing Name, also used as hostname, MQTT topic root and AP name
-#define INITIAL_AP_PASSWORD "smrtTHNG32"    // Default password of the AP, must be changed on first start, 
-                                            // but can be restored by the WiFi button
-#define SERIAL_BAUDS         115200         // Default baud rate for serial communication over USB
-#define RSSI_UPDATES         500            // Intervall in ms at which the RSSI value should be send as MQTT message
-#define STRING_LEN           64             // Bytes used to initalize char array. No path, topic, name, etc. should exceed this value
+/**************************************************************************/
+/*!
+  @brief  Struct to reference all needed pins
+*/
+/**************************************************************************/
+typedef struct
+{
+    int servoPul;
+    int servoDir;
+    int servoEna;
+    int servoAlm;
+    int servoPed;
+    int sw;
+    int lmt1;
+    int rgb;
+    int rs232txd;
+    int rs232rxd;
+} OSSMPins;
 
+/**************************************************************************/
+/*!
+  @brief  struct to reference other config parameters
+*/
+/**************************************************************************/
+typedef struct
+{
+    String description;
+    OSSMPins pin;
+    bool useIHSV57V6Driver;
+} OSSMConfig;
+
+/**************************************************************************/
+/*!
+  @brief  Pin definitions for OSSM Ref Board V2
+*/
+/**************************************************************************/
+static const OSSMPins OSSMv2{
+    .servoPul = 14,
+    .servoDir = 27,
+    .servoEna = 26,
+    .servoAlm = 13,
+    .servoPed = 4,
+    .sw = 23,
+    .lmt1 = 12,
+    .rgb = 25,
+    .rs232txd = 17,
+    .rs232rxd = 16};
+
+/**************************************************************************/
+/*!
+  @brief  Pin definitions for OSSM Ref Board V1
+*/
+/**************************************************************************/
+static const OSSMPins OSSMv1{
+    .servoPul = 14,
+    .servoDir = 27,
+    .servoEna = 26,
+    .servoAlm = 39,
+    .servoPed = 36,
+    .sw = 23,
+    .lmt1 = 12,
+    .rgb = 25,
+    .rs232txd = 17,
+    .rs232rxd = 16};
+
+/**************************************************************************/
+/*!
+  @brief  OSSM configurations with V1 and V2 board and generic stepper or
+  IHSV57 servo with firmware v6 for modbus control.
+*/
+/**************************************************************************/
+static const OSSMConfig genericOSSMv2{
+    .description = "OSSM Ref Board V2 with Generic Stepper",
+    .pin = OSSMv2,
+    .useIHSV57V6Driver = false};
+
+static const OSSMConfig genericOSSMv1{
+    .description = "OSSM Ref Board V1 with Generic Stepper",
+    .pin = OSSMv1,
+    .useIHSV57V6Driver = false};
+
+static const OSSMConfig ihsv57v6OSSMv2{
+    .description = "OSSM Ref Board V2 with IHSV57 v6 Servo",
+    .pin = OSSMv2,
+    .useIHSV57V6Driver = true};
+
+static const OSSMConfig ihsv57v6OSSMv1{
+    .description = "OSSM Ref Board V1 with IHSV57 v6 Servo",
+    .pin = OSSMv1,
+    .useIHSV57V6Driver = true};
+
+/**************************************************************************/
+/*!
+  @brief  Array holding all OSSM configurations
+*/
+/**************************************************************************/
+static const OSSMConfig OSSM[] = {
+    genericOSSMv2,
+    genericOSSMv1,
+    ihsv57v6OSSMv2,
+    ihsv57v6OSSMv1};
+
+static const unsigned int sizeOfOSSM = sizeof(OSSM) / sizeof(OSSM[0]);
+
+// // Pin Definitions OSSM Ref Board V2
+// #define SERVO_PUL         14
+// #define SERVO_DIR         27
+// #define SERVO_ENA         26
+// #define SERVO_ALM         13  // 39 on OSSM Ref Board V1
+// #define SERVO_PED         4   // 36 on OSSM Ref Board V1
+// #define SW                23
+// #define LMT1              12
+// #define RGB               25  //WS2813
+// #define RS232_TXD         17
+// #define RS232_RXD         16
--- firmware/FuckIO/src/main.cpp
+++ firmware/FuckIO/src/main.cpp
@@ -1,7 +1,7 @@
 /**
- *   FuckIO Firmware for ESP32
+ *   FuckIO Firmware for OSSM Ref Board V2
  *   Use MQTT messages to control all built-in motion patterns
- *   https://github.com/theelims/FuckIO 
+ *   https://github.com/theelims/FuckIO
  *
  * Copyright (C) 2023 theelims <elims@gmx.net>
  *
@@ -14,14 +14,20 @@
 #include <Arduino.h>
 #include "esp_log.h"
 #include <config.h>
-#include <housekeeping.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+// #include <ArduinoJson.h>
+// #include <ArduinoWebsockets.h>
+#include <NeoPixelBus.h>
 #include <StrokeEngine.h>
 #ifdef VIRTUAL
-  #include <motor/virtualMotor.h>
-#elif 
-  #include <motor/genericStepper.h>
+#include <motor/virtualMotor.h>
+#else
+#include <motor/genericStepper.h>
 #endif
+#include <LittleFS.h>
 
+#define SPIFFS LITTLEFS
 
 /*#################################################################################################
 ##
@@ -29,36 +35,50 @@
 ##
 ##################################################################################################*/
 
+// StrokeEngine ###################################################################################
 // Calculation Aid:
-#define STEP_PER_REV      2000      // How many steps per revolution of the motor (S1 off, S2 on, S3 on, S4 off)
-#define PULLEY_TEETH      20        // How many teeth has the pulley
-#define BELT_PITCH        2         // What is the timing belt pitch in mm
-#define MAX_RPM           3000.0    // Maximum RPM of motor
-#define STEP_PER_MM       STEP_PER_REV / (PULLEY_TEETH * BELT_PITCH)
-#define MAX_SPEED         (MAX_RPM / 60.0) * PULLEY_TEETH * BELT_PITCH
+#define STEP_PER_REV 2000 // How many steps per revolution of the motor (S1 off, S2 on, S3 on, S4 off)
+#define PULLEY_TEETH 20   // How many teeth has the pulley
+#define BELT_PITCH 2      // What is the timing belt pitch in mm
+#define MAX_RPM 3000.0    // Maximum RPM of motor
+#define STEP_PER_MM STEP_PER_REV / (PULLEY_TEETH * BELT_PITCH)
+#define MAX_SPEED (MAX_RPM / 60.0) * PULLEY_TEETH *BELT_PITCH
 
 #ifndef VIRTUAL
-  static motorProperties servoMotor {      
-    .stepsPerMillimeter = STEP_PER_MM,   
-    .invertDirection = true,      
-    .enableActiveLow = true,      
-    .stepPin = SERVO_PULSE,              
-    .directionPin = SERVO_DIR,          
-    .enablePin = SERVO_ENABLE              
-  }; 
+static motorProperties servoMotor{
+    .stepsPerMillimeter = STEP_PER_MM,
+    .invertDirection = true,
+    .enableActiveLow = true,
+    .stepPin = OSSM[0].pin.servoPul,
+    .directionPin = OSSM[0].pin.servoDir,
+    .enablePin = OSSM[0].pin.servoEna};
 #endif
 
 #ifdef VIRTUAL
-  VirtualMotor motor;
-#elif 
-  GenericStepperMotor motor;
+VirtualMotor motor;
+#else
+GenericStepperMotor motor;
 #endif
 
-//
 StrokeEngine Stroker;
 
-String getPatternJSON();
+// NeoPixel #######################################################################################
 
+#define colorSaturation 128
+
+NeoPixelBus<NeoGrbFeature, NeoEsp32I2s0800KbpsMethod> status(1, OSSM[0].pin.rgb); ////WS2813
+
+RgbColor red(colorSaturation, 0, 0);
+RgbColor green(0, colorSaturation, 0);
+RgbColor blue(0, 0, colorSaturation);
+RgbColor pink(colorSaturation, 0, colorSaturation);
+RgbColor white(colorSaturation);
+RgbColor black(0);
+
+// WebSocket Server ###############################################################################
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws/api");
 
 /*#################################################################################################
 ##
@@ -66,76 +86,158 @@ String getPatternJSON();
 ##
 ##################################################################################################*/
 
-void printSpeedPositionOnSerial(float time, float position, float speed) {
-  Serial.print(time, 2);
-  Serial.print("\t");
-  Serial.print(position, 2);
-  Serial.print("\t");
-  Serial.println(speed, 2);
+void printSpeedPositionOnSerial(unsigned int time, float position, float speed)
+{
+    String json = "{\"time\": ";
+    json += String(time);
+    json += ", \"position\": ";
+    json += String(position, 2);
+    json += ", \"speed\": ";
+    json += String(speed, 2);
+    json += "}";
+    ws.textAll(json);
 }
 
-void homingNotification(bool isHomed) {
-  if (isHomed) {
-    mqttNotify("Found home - Ready to rumble!");
-  } else {
-    mqttNotify("Homing failed!");
-  }
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+    if (type == WS_EVT_CONNECT)
+    {
+        // client connected
+        Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
+        client->printf("Hello Client %u :)", client->id());
+        client->ping();
+    }
+    else if (type == WS_EVT_DISCONNECT)
+    {
+        // client disconnected
+        Serial.printf("ws[%s][%u] disconnect: %u\n", server->url(), client->id());
+    }
+    else if (type == WS_EVT_ERROR)
+    {
+        // error was received from the other end
+        Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t *)arg), (char *)data);
+    }
+    else if (type == WS_EVT_PONG)
+    {
+        // pong message was received (in response to a ping request maybe)
+        Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char *)data : "");
+    }
+    else if (type == WS_EVT_DATA)
+    {
+        // data packet
+        AwsFrameInfo *info = (AwsFrameInfo *)arg;
+        if (info->final && info->index == 0 && info->len == len)
+        {
+            // the whole message is in a single frame and we got all of it's data
+            Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT) ? "text" : "binary", info->len);
+            if (info->opcode == WS_TEXT)
+            {
+                data[len] = 0;
+                Serial.printf("%s\n", (char *)data);
+            }
+            else
+            {
+                for (size_t i = 0; i < info->len; i++)
+                {
+                    Serial.printf("%02x ", data[i]);
+                }
+                Serial.printf("\n");
+            }
+            if (info->opcode == WS_TEXT)
+                client->text("I got your text message");
+            else
+                client->binary("I got your binary message");
+        }
+        else
+        {
+            // message is comprised of multiple frames or the frame is split into multiple packets
+            if (info->index == 0)
+            {
+                if (info->num == 0)
+                    Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
+                Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+            }
+
+            Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT) ? "text" : "binary", info->index, info->index + len);
+            if (info->message_opcode == WS_TEXT)
+            {
+                data[len] = 0;
+                Serial.printf("%s\n", (char *)data);
+            }
+            else
+            {
+                for (size_t i = 0; i < len; i++)
+                {
+                    Serial.printf("%02x ", data[i]);
+                }
+                Serial.printf("\n");
+            }
+
+            if ((info->index + len) == info->len)
+            {
+                Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+                if (info->final)
+                {
+                    Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
+                    if (info->message_opcode == WS_TEXT)
+                        client->text("I got your text message");
+                    else
+                        client->binary("I got your binary message");
+                }
+            }
+        }
+    }
 }
 
-void controlSpeed(String payload) {
-  Serial.println("Speed: " + String(payload));
-  Stroker.setParameter(StrokeParameter::RATE, payload.toFloat(), true);
-}
+// void homingNotification(bool isHomed) {
+//   if (isHomed) {
+//     mqttNotify("Found home - Ready to rumble!");
+//   } else {
+//     mqttNotify("Homing failed!");
+//   }
+// }
 
-void controlDepth(String payload) {
-  Serial.println("Depth: " + String(payload));
-  Stroker.setParameter(StrokeParameter::DEPTH, payload.toFloat(), true);
-}
+// void controlSpeed(String payload) {
+//   Serial.println("Speed: " + String(payload));
+//   Stroker.setParameter(StrokeParameter::RATE, payload.toFloat(), true);
+// }
 
-void controlStroke(String payload) {
-  Serial.println("Stroke: " + String(payload));
-  Stroker.setParameter(StrokeParameter::STROKE, payload.toFloat(), true);
-}
+// void controlDepth(String payload) {
+//   Serial.println("Depth: " + String(payload));
+//   Stroker.setParameter(StrokeParameter::DEPTH, payload.toFloat(), true);
+// }
 
-void controlSensation(String payload) {
-  Serial.println("Sensation: " + payload);
-  Stroker.setParameter(StrokeParameter::SENSATION, payload.toFloat(), true);
-}
+// void controlStroke(String payload) {
+//   Serial.println("Stroke: " + String(payload));
+//   Stroker.setParameter(StrokeParameter::STROKE, payload.toFloat(), true);
+// }
 
-void receiveCommand(String payload) {
-  Serial.println("Command: " + payload);
-  if (payload.equals("start")) {
-    Stroker.startPattern();
-  }
-  if (payload.equals("stop")) {
-    Stroker.stopMotion();
-  }
-  if (payload.equals("home")) {
-    //motor.home(homingNotification);
-    motor.home();
-  }
-  if (payload.equals("patternlist")) {
-    mqttPublish("/config", getPatternJSON());
-  }
-}
+// void controlSensation(String payload) {
+//   Serial.println("Sensation: " + payload);
+//   Stroker.setParameter(StrokeParameter::SENSATION, payload.toFloat(), true);
+// }
 
-void receivePWM(String payload) {
-  Serial.println("PWM: " + payload);
+// void receiveCommand(String payload) {
+//   Serial.println("Command: " + payload);
+//   if (payload.equals("start")) {
+//     Stroker.startPattern();
+//   }
+//   if (payload.equals("stop")) {
+//     Stroker.stopMotion();
+//   }
+//   if (payload.equals("home")) {
+//     //motor.home(homingNotification);
+//     motor.home();
+//   }
+//   if (payload.equals("patternlist")) {
+//     mqttPublish("/config", getPatternJSON());
+//   }
+// }
 
-  // Convert payload to int
-  int pulseWidth = payload.toInt();
-
-  // constrain to 8-bit value range
-  pulseWidth = constrain(pulseWidth, 0, 255);
-
-  // set PWM
-  ledcWrite(0, 0);
-}
-
-void receivePattern(String payload) {
-  Serial.println("Pattern Index: " + String(payload));
-  Stroker.setPattern(payload.toInt(), true);
-}
+// void receivePattern(String payload) {
+//   Serial.println("Pattern Index: " + String(payload));
+//   Stroker.setPattern(payload.toInt(), true);
+// }
 
 /*#################################################################################################
 ##
@@ -143,7 +245,7 @@ void receivePattern(String payload) {
 ##
 ##################################################################################################*/
 
-
+// None
 
 /*#################################################################################################
 ##
@@ -155,88 +257,158 @@ void receivePattern(String payload) {
 
 /*#################################################################################################
 ##
-##    M A I N   P R O G R A M
+##    F U N C T I O N S
 ##
 ##################################################################################################*/
 
-void setup() 
+/**
+ * Wait for WiFi connection, and, if not connected, reboot
+ */
+void waitForWiFiConnectOrReboot(bool printOnSerial = true)
 {
-  // Handle WiFi, Provisioning by IoTWebConf, MQTT connection & power in the background
-  ESP_LOGI("FuckIO", "Start WiFi provisioning");
-  beginHousework();
-
-  // Register callbacks for all MQTT subscriptions
-  ESP_LOGI("FuckIO", "Register MQTT subscriptions");
-  mqttSubscribe("/speed", controlSpeed);
-  mqttSubscribe("/depth", controlDepth);
-  mqttSubscribe("/stroke", controlStroke);
-  mqttSubscribe("/sensation", controlSensation);
-  mqttSubscribe("/command", receiveCommand);
-  mqttSubscribe("/pattern", receivePattern);
-  mqttSubscribe("/pwm", receivePWM);
-
-  // Set PWM output with 8bit resolution and 5kHz
-  ESP_LOGI("FuckIO", "Setup PWM output");
-  ledcSetup(0, 5000, 8);
-  ledcAttachPin(PWM, 0);
-  ledcWrite(0, 0);
-
-  // Wait for MQTT to be available
-  while (!mqttConnected()) {
-    delay(1000);
-  }
-  ESP_LOGI("FuckIO", "Connected to MQTT server");
-
-  // Wait a little bit for topic subscriptions to complete
-  delay(1000);
-
-  ESP_LOGI("FuckIO", "Configuring Motor");
-  
-#ifdef VIRTUAL
-  motor.begin(printSpeedPositionOnSerial, 50);
-#elif 
-  motor.begin(&servoMotor);
-  //motor.attachPositionFeedback(printSpeedPositionOnSerial, 50);
-  motor.setSensoredHoming(SERVO_ENDSTOP, INPUT_PULLUP, true);
-#endif
-  motor.setMaxSpeed(MAX_SPEED); // 2 m/s
-  motor.setMaxAcceleration(100000); // 100 m/s^2
-  motor.setMachineGeometry(160.0, 5.0);
-  
-  // Setup Stroke Stroker
-  ESP_LOGI("FuckIO", "Attach motor to StrokeEngine");
-  Stroker.attachMotor(&motor);
-  motor.enable();
-  motor.home();
-  //motor.home(homingNotification);
-
-  // Send available patterns as JSON
-  mqttPublish("/config", getPatternJSON());
+    uint32_t notConnectedCounter = 0;
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(100);
+        if (printOnSerial)
+        {
+            Serial.println("Wifi connecting...");
+        }
+        notConnectedCounter++;
+        if (notConnectedCounter > 50)
+        { // Reset board if not connected after 5s
+            if (printOnSerial)
+            {
+                Serial.println("Resetting due to Wifi not connecting...");
+            }
+            ESP.restart();
+        }
+    }
+    if (printOnSerial)
+    {
+        // Print wifi IP addess
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+    }
 }
 
-void loop() 
+String getPatternJSON()
 {
-  vTaskDelete(NULL);
-}
-
-/*#################################################################################################
-##
-##    O T H E R   F U N C T I O N S
-##
-##################################################################################################*/
-
-String getPatternJSON() {
     String JSON = "[{\"";
-    for (size_t i = 0; i < Stroker.getNumberOfPattern(); i++) {
+    for (size_t i = 0; i < Stroker.getNumberOfPattern(); i++)
+    {
         JSON += String(Stroker.getPatternName(i));
         JSON += "\": ";
         JSON += String(i, DEC);
-        if (i < Stroker.getNumberOfPattern() - 1) {
+        if (i < Stroker.getNumberOfPattern() - 1)
+        {
             JSON += "},{\"";
-        } else {
+        }
+        else
+        {
             JSON += "}]";
         }
     }
     Serial.println(JSON);
     return JSON;
+}
+
+/*#################################################################################################
+##
+##    M A I N   P R O G R A M
+##
+##################################################################################################*/
+
+void setup()
+{
+
+    // Configure recovery pin as input
+    pinMode(OSSM[0].pin.sw, INPUT_PULLDOWN);
+
+    // start Neopixel light show
+    status.Begin();
+
+    // start serial output
+    Serial.begin(115200);
+    while (!Serial)
+        ; // wait for serial attach
+
+    Serial.println();
+    Serial.println("Initializing...");
+    Serial.flush();
+
+    status.SetPixelColor(0, red);
+    status.Show();
+
+    // Initialize LittleFS and halt if not flashed
+    if (!LittleFS.begin(false))
+    {
+        ESP_LOGE("FuckIO", "Failed to mount file system! Please upload filesystem image.");
+        while (true)
+            ;
+    }
+
+    // Handle WiFi, Provisioning by IoTWebConf, MQTT connection & power in the background
+    ESP_LOGI("FuckIO", "Start WiFi");
+    WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE);
+    WiFi.setHostname("fuckio");
+    WiFi.begin("Kellerkind", "Putzplan");
+    waitForWiFiConnectOrReboot();
+    status.SetPixelColor(0, green);
+    status.Show();
+
+    Serial.println();
+    Serial.println("Running...");
+
+    // Register callbacks for all MQTT subscriptions
+    // ESP_LOGI("FuckIO", "Register MQTT subscriptions");
+    // mqttSubscribe("/speed", controlSpeed);
+    // mqttSubscribe("/depth", controlDepth);
+    // mqttSubscribe("/stroke", controlStroke);
+    // mqttSubscribe("/sensation", controlSensation);
+    // mqttSubscribe("/command", receiveCommand);
+    // mqttSubscribe("/pattern", receivePattern);
+
+    ESP_LOGI("FuckIO", "WebSocket Server available on port 80");
+    // Start webserver
+    ws.onEvent(onEvent);
+    server.addHandler(&ws);
+    server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
+    server.begin();
+
+    // Wait a little bit for topic subscriptions to complete
+    delay(1000);
+
+    ESP_LOGI("FuckIO", "Configuring Motor");
+
+#ifdef VIRTUAL
+    motor.begin(printSpeedPositionOnSerial, 50);
+#else
+    motor.begin(&servoMotor);
+    // motor.attachPositionFeedback(printSpeedPositionOnSerial, 50);
+    motor.setSensoredHoming(OSSM[0].pin.lmt1, INPUT_PULLUP, true);
+#endif
+    motor.setMaxSpeed(MAX_SPEED);     // 2 m/s
+    motor.setMaxAcceleration(100000); // 100 m/s^2
+    motor.setMachineGeometry(160.0, 5.0);
+
+    Stroker.attachMotor(&motor);
+    motor.enable();
+    motor.home();
+    // motor.home(homingNotification);
+
+    // Send available patterns as JSON
+    // mqttPublish("/config", getPatternJSON());
+
+    // Wait a little bit
+    delay(1000);
+
+    Stroker.setParameter(StrokeParameter::SENSATION, 30.0, true);
+    Stroker.startPattern();
+}
+
+void loop()
+{
+    ws.cleanupClients(); // <-- add this line
+    delay(100);
 }
